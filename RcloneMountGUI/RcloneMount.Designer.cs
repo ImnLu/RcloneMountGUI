77e92eb422a7bd37d7cb6ce420469cd600b2cc37
Mali <nlu@outlook.com.tr>
2022-07-30 00:54:23 +0300

    Add project files.

---
+++ RcloneMountGUI/RcloneMount.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace RcloneMountGUI
+{
+    partial class RcloneMount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RcloneMount));
+            this.txtRLocation = new System.Windows.Forms.TextBox();
+            this.lblRloneLocation = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.openFileDialogRclone = new System.Windows.Forms.OpenFileDialog();
+            this.btnMount = new System.Windows.Forms.Button();
+            this.lblMountOptions = new System.Windows.Forms.Label();
+            this.comboBoxMount = new System.Windows.Forms.ComboBox();
+            this.lblRemoteName = new System.Windows.Forms.Label();
+            this.txtRemoteName = new System.Windows.Forms.TextBox();
+            this.lblDriveLetter = new System.Windows.Forms.Label();
+            this.txtDriveLetter = new System.Windows.Forms.TextBox();
+            this.btnUnmount = new System.Windows.Forms.Button();
+            this.linklblDocument = new System.Windows.Forms.LinkLabel();
+            this.checkBoxRAAS = new System.Windows.Forms.CheckBox();
+            this.notifyIconRclone = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // txtRLocation
+            // 
+            this.txtRLocation.Location = new System.Drawing.Point(143, 16);
+            this.txtRLocation.Margin = new System.Windows.Forms.Padding(2);
+            this.txtRLocation.Name = "txtRLocation";
+            this.txtRLocation.ReadOnly = true;
+            this.txtRLocation.Size = new System.Drawing.Size(258, 26);
+            this.txtRLocation.TabIndex = 0;
+            this.txtRLocation.TabStop = false;
+            // 
+            // lblRloneLocation
+            // 
+            this.lblRloneLocation.AutoSize = true;
+            this.lblRloneLocation.Location = new System.Drawing.Point(11, 19);
+            this.lblRloneLocation.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRloneLocation.Name = "lblRloneLocation";
+            this.lblRloneLocation.Size = new System.Drawing.Size(128, 20);
+            this.lblRloneLocation.TabIndex = 100;
+            this.lblRloneLocation.Text = "Rclone Location:";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSelect.Location = new System.Drawing.Point(405, 16);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(69, 26);
+            this.btnSelect.TabIndex = 10;
+            this.btnSelect.TabStop = false;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // openFileDialogRclone
+            // 
+            this.openFileDialogRclone.FileName = "rclone";
+            this.openFileDialogRclone.Filter = "Exe Files (.exe) |* .exe";
+            // 
+            // btnMount
+            // 
+            this.btnMount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnMount.Location = new System.Drawing.Point(143, 155);
+            this.btnMount.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMount.Name = "btnMount";
+            this.btnMount.Size = new System.Drawing.Size(155, 35);
+            this.btnMount.TabIndex = 11;
+            this.btnMount.TabStop = false;
+            this.btnMount.Text = "Mount";
+            this.btnMount.UseVisualStyleBackColor = true;
+            this.btnMount.Click += new System.EventHandler(this.btnMount_Click);
+            // 
+            // lblMountOptions
+            // 
+            this.lblMountOptions.AutoSize = true;
+            this.lblMountOptions.Location = new System.Drawing.Point(21, 109);
+            this.lblMountOptions.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMountOptions.Name = "lblMountOptions";
+            this.lblMountOptions.Size = new System.Drawing.Size(117, 20);
+            this.lblMountOptions.TabIndex = 103;
+            this.lblMountOptions.Text = "Mount Options:";
+            // 
+            // comboBoxMount
+            // 
+            this.comboBoxMount.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMount.Items.AddRange(new object[] {
+            "--vfs-cache-mode writes",
+            "--vfs-cache-mode off",
+            "--vfs-cache-mode minimal",
+            "--vfs-cache-mode full"});
+            this.comboBoxMount.Location = new System.Drawing.Point(143, 106);
+            this.comboBoxMount.Name = "comboBoxMount";
+            this.comboBoxMount.Size = new System.Drawing.Size(331, 28);
+            this.comboBoxMount.TabIndex = 3;
+            this.comboBoxMount.TabStop = false;
+            // 
+            // lblRemoteName
+            // 
+            this.lblRemoteName.AutoSize = true;
+            this.lblRemoteName.Location = new System.Drawing.Point(23, 49);
+            this.lblRemoteName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRemoteName.Name = "lblRemoteName";
+            this.lblRemoteName.Size = new System.Drawing.Size(116, 20);
+            this.lblRemoteName.TabIndex = 101;
+            this.lblRemoteName.Text = "Remote Name:";
+            // 
+            // txtRemoteName
+            // 
+            this.txtRemoteName.Location = new System.Drawing.Point(143, 46);
+            this.txtRemoteName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtRemoteName.Name = "txtRemoteName";
+            this.txtRemoteName.Size = new System.Drawing.Size(331, 26);
+            this.txtRemoteName.TabIndex = 1;
+            this.txtRemoteName.TabStop = false;
+            // 
+            // lblDriveLetter
+            // 
+            this.lblDriveLetter.AutoSize = true;
+            this.lblDriveLetter.Location = new System.Drawing.Point(43, 80);
+            this.lblDriveLetter.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDriveLetter.Name = "lblDriveLetter";
+            this.lblDriveLetter.Size = new System.Drawing.Size(95, 20);
+            this.lblDriveLetter.TabIndex = 102;
+            this.lblDriveLetter.Text = "Drive Letter:";
+            // 
+            // txtDriveLetter
+            // 
+            this.txtDriveLetter.Location = new System.Drawing.Point(143, 76);
+            this.txtDriveLetter.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDriveLetter.MaxLength = 1;
+            this.txtDriveLetter.Name = "txtDriveLetter";
+            this.txtDriveLetter.Size = new System.Drawing.Size(22, 26);
+            this.txtDriveLetter.TabIndex = 2;
+            this.txtDriveLetter.TabStop = false;
+            this.txtDriveLetter.Text = "Z";
+            // 
+            // btnUnmount
+            // 
+            this.btnUnmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUnmount.Location = new System.Drawing.Point(319, 155);
+            this.btnUnmount.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUnmount.Name = "btnUnmount";
+            this.btnUnmount.Size = new System.Drawing.Size(155, 35);
+            this.btnUnmount.TabIndex = 12;
+            this.btnUnmount.TabStop = false;
+            this.btnUnmount.Text = "Unmount";
+            this.btnUnmount.UseVisualStyleBackColor = true;
+            this.btnUnmount.Click += new System.EventHandler(this.btnUnmount_Click);
+            // 
+            // linklblDocument
+            // 
+            this.linklblDocument.AutoSize = true;
+            this.linklblDocument.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.linklblDocument.Location = new System.Drawing.Point(395, 90);
+            this.linklblDocument.Name = "linklblDocument";
+            this.linklblDocument.Size = new System.Drawing.Size(79, 13);
+            this.linklblDocument.TabIndex = 104;
+            this.linklblDocument.TabStop = true;
+            this.linklblDocument.Text = "Documentation";
+            // 
+            // checkBoxRAAS
+            // 
+            this.checkBoxRAAS.AutoSize = true;
+            this.checkBoxRAAS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBoxRAAS.Location = new System.Drawing.Point(15, 220);
+            this.checkBoxRAAS.Name = "checkBoxRAAS";
+            this.checkBoxRAAS.Size = new System.Drawing.Size(186, 24);
+            this.checkBoxRAAS.TabIndex = 105;
+            this.checkBoxRAAS.Text = "Auto Mount at Startup";
+            this.checkBoxRAAS.UseVisualStyleBackColor = true;
+            this.checkBoxRAAS.CheckedChanged += new System.EventHandler(this.checkBoxRAAS_CheckedChanged);
+            // 
+            // notifyIconRclone
+            // 
+            this.notifyIconRclone.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIconRclone.BalloonTipText = "Mounted Succesfully";
+            this.notifyIconRclone.BalloonTipTitle = "Rclone Mount";
+            this.notifyIconRclone.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconRclone.Icon")));
+            this.notifyIconRclone.Text = "Rclone Mount";
+            this.notifyIconRclone.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIconRclone_MouseDoubleClick);
+            // 
+            // RcloneMount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 251);
+            this.Controls.Add(this.checkBoxRAAS);
+            this.Controls.Add(this.linklblDocument);
+            this.Controls.Add(this.btnUnmount);
+            this.Controls.Add(this.txtDriveLetter);
+            this.Controls.Add(this.lblDriveLetter);
+            this.Controls.Add(this.txtRemoteName);
+            this.Controls.Add(this.lblRemoteName);
+            this.Controls.Add(this.comboBoxMount);
+            this.Controls.Add(this.lblMountOptions);
+            this.Controls.Add(this.btnMount);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.lblRloneLocation);
+            this.Controls.Add(this.txtRLocation);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "RcloneMount";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rclone Mount";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.RcloneMount_FormClosed);
+            this.Load += new System.EventHandler(this.RcloneMount_Load);
+            this.Resize += new System.EventHandler(this.RcloneMount_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRLocation;
+        private System.Windows.Forms.Label lblRloneLocation;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.OpenFileDialog openFileDialogRclone;
+        private System.Windows.Forms.Button btnMount;
+        private System.Windows.Forms.Label lblMountOptions;
+        private System.Windows.Forms.ComboBox comboBoxMount;
+        private System.Windows.Forms.Label lblRemoteName;
+        private System.Windows.Forms.TextBox txtRemoteName;
+        private System.Windows.Forms.Label lblDriveLetter;
+        private System.Windows.Forms.TextBox txtDriveLetter;
+        private System.Windows.Forms.Button btnUnmount;
+        private System.Windows.Forms.LinkLabel linklblDocument;
+        private System.Windows.Forms.CheckBox checkBoxRAAS;
+        private System.Windows.Forms.NotifyIcon notifyIconRclone;
+    }
+}
+
---
+++ RcloneMountGUI/RcloneMount.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+using System.Security.Cryptography;
+using System.Net;
+
+namespace RcloneMountGUI
+{
+    public partial class RcloneMount : Form
+    {
+        public RcloneMount()
+        {
+            InitializeComponent();
+            TrayMenuContext();
+
+            // Default selection
+            comboBoxMount.Text = "--vfs-cache-mode writes";
+
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+
+                if (key.GetValue("RcloneMountGUI").ToString() == "\"" + Application.ExecutablePath + "\"" + " -tray")
+                {
+                    checkBoxRAAS.Checked = true;
+                }
+            }
+            catch { }
+
+            loadSettings();
+        }
+
+        string[] args = Environment.GetCommandLineArgs();
+
+        #region Variables
+
+        string rclonePath, rcloneFileName;
+
+        #endregion
+
+        #region Methods
+
+        private void loadSettings()
+        {
+            // Loads settings from registry
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\RcloneMountGUI", true);
+
+            if (key != null)
+            {
+                try
+                {
+                    if (!String.IsNullOrEmpty(key.GetValue("RcloneLocation").ToString()))
+                    {
+                        txtRLocation.Text = key.GetValue("RcloneLocation").ToString();
+
+                        rclonePath = Path.GetDirectoryName(txtRLocation.Text);
+                        rcloneFileName = Path.GetFileNameWithoutExtension(txtRLocation.Text);
+                    }
+                }
+                catch { }
+
+                try
+                {
+                    if (!String.IsNullOrEmpty(key.GetValue("RemoteName").ToString()))
+                    {
+                        txtRemoteName.Text = key.GetValue("RemoteName").ToString();
+                    }
+                }
+                catch { }
+
+                try
+                {
+                    if (!String.IsNullOrEmpty(key.GetValue("DriveLetter").ToString()))
+                    {
+                        txtDriveLetter.Text = key.GetValue("DriveLetter").ToString();
+                    }
+                }
+                catch { }
+                try
+                {
+                    if (!String.IsNullOrEmpty(key.GetValue("MountOptions").ToString()))
+                    {
+                        comboBoxMount.Text = key.GetValue("MountOptions").ToString();
+                    }
+                }
+                catch { }
+            }
+            else
+                Registry.CurrentUser.CreateSubKey("SOFTWARE\\RcloneMountGUI");
+        }
+
+        private void runTray()
+        {
+            // Minimizes to tray
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIconRclone.Visible = true;
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void RcloneStart()
+        {
+            // Starts the mounting process
+            Process process = new Process();
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/C " + rcloneFileName + " mount " + txtRemoteName.Text + ": " + txtDriveLetter.Text + ": " + comboBoxMount.Text;
+            process.StartInfo.WorkingDirectory = rclonePath;
+            process.Start();
+
+            System.Threading.Thread.Sleep(1000);
+
+            // Check if mounted
+            if (this.WindowState != FormWindowState.Minimized)
+            {
+                Process[] processes = Process.GetProcesses();
+                foreach (Process process2 in processes)
+                {
+                    if (process2.ProcessName == rcloneFileName)
+                    {
+                        MessageBox.Show("Mounted successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+        private void RcloneKill()
+        {
+            // Kills the process
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                if (process.ProcessName == rcloneFileName)
+                {
+                    process.Kill();
+                    MessageBox.Show("Unmounted successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void TrayMenuContext()
+        {
+            this.notifyIconRclone.ContextMenuStrip = new ContextMenuStrip();
+            this.notifyIconRclone.ContextMenuStrip.Items.Add("Show", null, this.Show_Click);
+            this.notifyIconRclone.ContextMenuStrip.Items.Add("Exit", null, this.Exit_Click);
+        }
+
+        private string calculateMd5(string file)
+        {
+            // Calculates the md5 code of the file
+            using (var md5Instance = MD5.Create())
+            {
+                using (var stream = File.OpenRead(file))
+                {
+                    var hashResult = md5Instance.ComputeHash(stream);
+                    return BitConverter.ToString(hashResult).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+
+        private bool checkNewVersion(string md5Link)
+        {
+            // Checks for a new version
+
+            WebClient client = new WebClient();
+
+            string appName = Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            string appCurrentVersion = calculateMd5(appName);
+
+            string latestVersion = client.DownloadString(md5Link);
+
+            if (appCurrentVersion == latestVersion || appCurrentVersion + "\n" == latestVersion)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void updateApp(string appLink)
+        {
+            // Updates the application
+
+            WebClient client = new WebClient();
+
+            string appName = Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            string tempFolder = Path.GetTempPath();
+
+            client.DownloadFile(appLink, tempFolder + appName);
+
+            if (File.Exists(tempFolder + appName + ".bak"))
+                File.Delete(tempFolder + appName + ".bak");
+
+            File.Move(appName, tempFolder + appName + ".bak");
+            File.Move(tempFolder + appName, tempFolder);
+        }
+
+        #endregion
+
+        #region Form tools
+
+        private void RcloneMount_Load(object sender, EventArgs e)
+        {
+            // If there is a new version of the application, it will ask for an update
+            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                if (checkNewVersion("md5Link"))
+                {
+                    if (MessageBox.Show("A new version of the app is available. Would you like to update?", "Update", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.OK)
+                    {
+                        updateApp("appLink");
+                    }
+                }
+            }
+
+            // Starts the application according to the argument
+            if (args.Length > 1)
+            {
+                if (!String.IsNullOrEmpty(args[1]))
+                {
+                    if (args[1] == "-tray")
+                    {
+                        this.WindowState = FormWindowState.Minimized;
+
+                        runTray();
+                        RcloneStart();
+                    }
+                }
+            }
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            // Saves requirement information
+            if (openFileDialogRclone.ShowDialog() == DialogResult.OK)
+            {
+                txtRLocation.Text = openFileDialogRclone.FileName;
+                rclonePath = Path.GetDirectoryName(openFileDialogRclone.FileName);
+                rcloneFileName = Path.GetFileNameWithoutExtension(openFileDialogRclone.SafeFileName);
+            }
+        }
+
+        private void btnMount_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtRLocation.Text) || String.IsNullOrEmpty(txtRemoteName.Text) || String.IsNullOrEmpty(txtDriveLetter.Text))
+            {
+                MessageBox.Show("Please fill in the required information.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                // Saves settings
+                RegistryKey key;
+                key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\RcloneMountGUI", true);
+
+                key.SetValue("RcloneLocation", txtRLocation.Text);
+                key.SetValue("RemoteName", txtRemoteName.Text);
+                key.SetValue("DriveLetter", txtDriveLetter.Text);
+                key.SetValue("MountOptions", comboBoxMount.Text);
+
+                RcloneStart();
+            }
+        }
+
+        private void btnUnmount_Click(object sender, EventArgs e)
+        {
+            RcloneKill();
+        }
+
+        private void checkBoxRAAS_CheckedChanged(object sender, EventArgs e)
+        {
+            // Run automatically at startup settings
+            if (checkBoxRAAS.Checked)
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                key.SetValue("RcloneMountGUI", "\"" + Application.ExecutablePath + "\"" + " -tray");
+            }
+            else
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                key.DeleteValue("RcloneMountGUI");
+            }
+        }
+
+        private void RcloneMount_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIconRclone.Visible = true;
+            }
+        }
+
+        private void notifyIconRclone_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIconRclone.Visible = false;
+        }
+
+        private void Show_Click(object sender, EventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIconRclone.Visible = false;
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            RcloneKill();
+            Application.Exit();
+        }
+
+        private void RcloneMount_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            RcloneKill();
+            Application.Exit();
+        }
+
+        #endregion
+    }
+}
